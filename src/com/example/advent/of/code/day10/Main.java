0fc3c4f507565279587c897cba70cfdff9aae35b
koniczyn5 <koniczyn5@tlen.pl>
2021-12-11 19:49:31 +0100

    Day 10 refactored.

--- src/com/example/advent/of/code/day10/Main.java
+++ src/com/example/advent/of/code/day10/Main.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 import java.util.Stack;
 
 public class Main {
@@ -25,69 +26,52 @@ public class Main {
             e.printStackTrace();
         }
 
-        int resultPart1 = 0;
+        List<Character> openingBrackets = List.of('(','[','{','<');
+        Map<Character, Character> closingToOpening = Map.of(
+                ')', '(',
+                ']', '[',
+                '}', '{',
+                '>', '<');
+        Map<Character, Integer> closingBracketToPoint = Map.of(
+                ')', 3,
+                ']', 57,
+                '}', 1197,
+                '>', 25137);
+        Map<Character, Integer> openingBracketToPoint = Map.of(
+                '(', 1,
+                '[', 2,
+                '{', 3,
+                '<', 4);
 
+        int resultPart1 = 0;
         List<Long> autocompleteResults = new ArrayList<>();
 
         for (String line : inputs) {
-            Stack<Character> openingBrackets = new Stack<>();
+            Stack<Character> brackets = new Stack<>();
             boolean isLineCorrupted = false;
             //part 1
             for (int i = 0; i < line.length(); i++) {
-                if (line.charAt(i) == '('
-                        || line.charAt(i) == '['
-                        || line.charAt(i) == '{'
-                        || line.charAt(i) == '<') {
-                    openingBrackets.push(line.charAt(i));
-                } else if (line.charAt(i) == ')') {
-                    if (openingBrackets.pop() != '(') {
-                        resultPart1 += 3;
-                        isLineCorrupted = true;
-                        break;
-                    }
-                } else if (line.charAt(i) == ']') {
-                    if (openingBrackets.pop() != '[') {
-                        resultPart1 += 57;
-                        isLineCorrupted = true;
-                        break;
-                    }
-                } else if (line.charAt(i) == '}') {
-                    if (openingBrackets.pop() != '{') {
-                        resultPart1 += 1197;
-                        isLineCorrupted = true;
-                        break;
-                    }
-                } else if (line.charAt(i) == '>') {
-                    if (openingBrackets.pop() != '<') {
-                        resultPart1 += 25137;
-                        isLineCorrupted = true;
-                        break;
-                    }
+                if (openingBrackets.contains(line.charAt(i))) {
+                    brackets.push(line.charAt(i));
+                } else if (brackets.pop() != closingToOpening.get(line.charAt(i))) {
+                    resultPart1 += closingBracketToPoint.get(line.charAt(i));
+                    isLineCorrupted = true;
+                    break;
                 }
             }
 
             //part 2
             if (!isLineCorrupted) {
                 long autoCompleteResult = 0;
-                while (!openingBrackets.empty()) {
-                    Character bracket = openingBrackets.pop();
+                while (!brackets.empty()) {
                     autoCompleteResult *= 5;
-                    if (bracket == '(') {
-                        autoCompleteResult += 1;
-                    } else if (bracket == '[') {
-                        autoCompleteResult += 2;
-                    } else if (bracket == '{') {
-                        autoCompleteResult += 3;
-                    } else if (bracket == '<') {
-                        autoCompleteResult += 4;
-                    }
+                    autoCompleteResult += openingBracketToPoint.get(brackets.pop());
                 }
                 autocompleteResults.add(autoCompleteResult);
             }
         }
 
         Collections.sort(autocompleteResults);
-
         long resultPart2 = autocompleteResults.get((autocompleteResults.size() - 1) / 2);
 
         System.out.println(resultPart1);
