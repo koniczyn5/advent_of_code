9aef3fafe418d437fef4040baa9e6628b5eadd8e
koniczyn5 <koniczyn5@tlen.pl>
2021-12-12 12:57:25 +0100

    Day 12 part 2.

--- src/com/example/advent/of/code/day12/Main.java
+++ src/com/example/advent/of/code/day12/Main.java
@@ -33,14 +33,14 @@ public class Main {
         Map<String, Cave> caves = new HashMap<>();
 
         //Create cave system
-        for (String line: inputs) {
+        for (String line : inputs) {
             int delimiterPos = line.indexOf('-');
             String firstCaveName = line.substring(0, delimiterPos);
-            String secondCaveName = line.substring(delimiterPos+1);
-            if(!caves.containsKey(firstCaveName)) {
+            String secondCaveName = line.substring(delimiterPos + 1);
+            if (!caves.containsKey(firstCaveName)) {
                 caves.put(firstCaveName, new Cave(firstCaveName));
             }
-            if(!caves.containsKey(secondCaveName)) {
+            if (!caves.containsKey(secondCaveName)) {
                 caves.put(secondCaveName, new Cave(secondCaveName));
             }
             Cave firstCave = caves.get(firstCaveName);
@@ -49,27 +49,34 @@ public class Main {
             secondCave.addConnection(firstCave);
         }
 
-        long resultPart2 = 0;
-
         List<Cave> startingPath = new ArrayList<>();
         startingPath.add(caves.get(START));
-        int resultPart1 = goThroughCaves(startingPath);
+        int resultPart1 = goThroughCaves(startingPath, true);
 
         System.out.println(resultPart1);
+
+        int resultPart2 = goThroughCaves(startingPath, false);
 
         System.out.println(resultPart2);
     }
 
-    private static int goThroughCaves(List<Cave> currentPath) {
+    private static int goThroughCaves(List<Cave> currentPath, boolean wasAnySmallCaveVisitedTwice) {
         int pathsThrough = 0;
-        for (Cave connectedCave : currentPath.get(currentPath.size()-1).getConnections()) {
-            if (connectedCave.getName().equals(END)) {
-                ++pathsThrough;
-            } else if (connectedCave.isBigCave()
-            || currentPath.stream().noneMatch(cave -> cave.equals(connectedCave))) {
-                List<Cave> newPath = new ArrayList<>(currentPath);
-                newPath.add(connectedCave);
-                pathsThrough += goThroughCaves(newPath);
+        for (Cave connectedCave : currentPath.get(currentPath.size() - 1).getConnections()) {
+            if (!connectedCave.getName().equals(START)) {
+                if (connectedCave.getName().equals(END)) {
+                    ++pathsThrough;
+                } else if (connectedCave.isBigCave()
+                        || currentPath.stream().noneMatch(cave -> cave.equals(connectedCave))
+                ) {
+                    List<Cave> newPath = new ArrayList<>(currentPath);
+                    newPath.add(connectedCave);
+                    pathsThrough += goThroughCaves(newPath, wasAnySmallCaveVisitedTwice);
+                } else if (!wasAnySmallCaveVisitedTwice) {
+                    List<Cave> newPath = new ArrayList<>(currentPath);
+                    newPath.add(connectedCave);
+                    pathsThrough += goThroughCaves(newPath, true);
+                }
             }
         }
         return pathsThrough;
